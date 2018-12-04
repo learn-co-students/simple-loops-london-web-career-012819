12adec7fea776f458ea336b52cba078b5ee43fef
Alex Bujenita <dekc89@gmail.com>
2018-12-04 21:45:11 +0000

    Loops done

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,42 +1,52 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times)
+def loop_iterator(x)
+  cntr = 0
   # Code your solution here using the "loop" keyword to puts out the below phrase
-  # This and all the other methods take an argument of an integer 
+  # This and all the other methods take an argument of an integer
   # The integer is the number of times the loops should puts out the phrase
-  # How can we make sure the loop breaks once it has puts out the phrase the 
+  # How can we make sure the loop breaks once it has puts out the phrase the
   #   correct number of times?
-  # Maybe we should keep count of the number of times we've puts out the 
+  # Maybe we should keep count of the number of times we've puts out the
   #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  loop do
+    puts "Welcome to Flatiron School's Web Development Course!"
+    cntr += 1
+    break if cntr == x
+  end
 end
 
-def times_iterator(number_of_times)
+def times_iterator(x)
   # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  x.times do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
-def while_iterator(number_of_times)
+def while_iterator(x)
+  c = 0
   # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  # hint: use a counter to tell the while loop when to stop!
+  while c != x
+    puts "Welcome to Flatiron School's Web Development Course!"
+    c+=1
+  end
 end
 
-def until_iterator(number_of_times)
+def until_iterator(x)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  c = 0
+  until c == x
+    puts "Welcome to Flatiron School's Web Development Course!"
+    c += 1
+  end
 end
 
-def for_iterator(number_of_times)
+def for_iterator(x)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for c in 1..x
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
-
